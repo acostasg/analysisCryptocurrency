0b107fddc783bbe54f4f2c1729440679764a9785
Albert Costas <albert.costas@privalia.com>
2017-10-13 01:24:24 +0200

    Add new url to crypto currencues

---
+++ projects/scraping_crypto_currencies.py
@@ -0,0 +1,98 @@
+import csv
+import datetime
+import os
+
+import BeautifulSoup as bs
+import urllib3 as urllib
+
+UTF8 = 'utf-8'
+DOWN = 'baixada'
+UP = 'pujada'
+UP_NAME_GIF = '/imag3/fsube2.gif'
+
+URLS = [
+    ['/all/views/all/', 'cryoto_currencies'],
+]
+BASE_URL = 'https://coinmarketcap.com'
+
+
+def start_scraping():
+    urllib.disable_warnings()
+
+    for url in URLS:
+        scraping_url(BASE_URL + url[0], url[1])
+
+    print('Success crypto currencies!!!')
+
+
+def scraping_url(url, name):
+    rows = []
+
+    http = urllib.PoolManager()
+
+    response = http.request('GET', url)
+
+    if response.status != 200:
+        print("Invalid url " + url)
+        return
+
+    html = response.data.decode(UTF8)
+
+    if html == "":
+        print("Error empty html for " + url)
+        return
+
+    soup = bs.BeautifulSoup(html)
+
+    table_list = soup.find('table', attrs={'id': 'currencies-all'}).find('tbody')
+    trs = table_list.findAll('tr')
+
+    append_title_row(rows)
+
+    print('Init scraping in url ' + url)
+
+    for tr in trs:
+        row = []
+
+        for td in tr.findAll('td'):
+            append_text(row, td)
+
+        rows.append(
+            row
+        )
+
+    now = datetime.datetime.now()
+
+    with open(get_file_name(name, now), 'w') as f_output:
+        csv_output = csv.writer(f_output)
+        csv_output.writerows(rows)
+
+    print('Finish url' + url)
+
+
+def append_text(row, td):
+    row.append(td.text.encode(UTF8))
+
+
+def append_title_row(rows):
+    rows.append(
+        [
+            "Numero",
+            "Nom",
+            "Simbol",
+            "Cap de mercat",
+            "Preu",
+            "Oferta circulant",
+            "Volum 24 hores",
+            "% 1h",
+            "% 2h",
+            "% 7d"
+        ]
+    )
+
+
+def get_file_name(name, now):
+    dir = './output/crypoto_currencies/' + now.day.__str__() + '_' + now.month.__str__() + '_' + now.year.__str__()
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+    return dir + '/' + name + '.csv'
---
+++ projects/scraping_stock_indexs.py
@@ -0,0 +1,121 @@
+import csv
+import datetime
+import os
+
+import BeautifulSoup as bs
+import urllib3 as urllib
+
+UTF8 = 'utf-8'
+DOWN = 'baixada'
+UP = 'pujada'
+UP_NAME_GIF = '/imag3/fsube2.gif'
+
+URLS = [
+    ['IBEX-35', 'ibex_35'],
+    ['mercado-continuo', 'mercat_continuo'],
+    ['IGBM', 'igbm'],
+    ['ECO10', 'eco10'],
+    ['IBEX-DIVIDENDO', 'ibex_divident'],
+]
+BASE_URL = 'http://www.eleconomista.es/indice/'
+
+
+def start_scraping():
+    for url in URLS:
+        scraping_url(BASE_URL + url[0], url[1])
+
+    print('Success stock indexs!!!')
+
+
+def scraping_url(url, name):
+    rows = []
+
+    http = urllib.PoolManager()
+
+    response = http.request('GET', url)
+
+    if response.status != 200:
+        print("Invalid url " + url)
+        return
+
+    html = response.data.decode(UTF8)
+
+    if html == "":
+        print("Error empty html for " + url)
+        return
+
+    soup = bs.BeautifulSoup(html)
+
+    table_list = soup.find('table', attrs={'class': 'tablalista'}).find('tbody')
+    trs = table_list.findAll('tr')
+
+    # first trs is titles
+    trs.pop(0)
+
+    # last trs is invalid
+    trs.pop(len(trs) - 1)
+
+    append_title_row(rows)
+
+    print('Init scraping in url '+ url)
+
+    for tr in trs:
+        row = []
+
+        for td in tr.findAll('td'):
+            append_text(row, td)
+
+        rows.append(
+            row
+        )
+
+    now = datetime.datetime.now()
+
+    with open(get_file_name(name, now), 'w') as f_output:
+        csv_output = csv.writer(f_output)
+        csv_output.writerows(rows)
+
+    print('Finish url' + url)
+
+
+def append_text(row, td):
+    text = td.text.encode(UTF8)
+    if text:
+        row.append(text)
+    else:
+        get_status_name(row, td)
+
+
+def get_status_name(row, td):
+    if td.find('img', attrs={'src': UP_NAME_GIF}):
+        row.append(UP)
+    else:
+        row.append(DOWN)
+
+
+def append_title_row(rows):
+    rows.append(
+        [
+            "Nom",
+            "Preu",
+            "Estat",
+            "Var. per cent",
+            "Var. en euros",
+            "Volum en euros",
+            "Capitalizacio (milions euros)",
+            "Per",
+            "Rent./Div",
+            "Hora"
+        ]
+    )
+
+
+def get_file_name(name, now):
+    dir = './output/stock_index/' + now.day.__str__() + '_' + now.month.__str__() + '_' + now.year.__str__()
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+    return dir + '/' + name + '.csv'
+
+
+if __name__ == "__main__":
+    start_scraping()
