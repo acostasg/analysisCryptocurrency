812e66a9fd361e2d763e2c8b986778abced31d69
Albert <acostasg@uoc.edu>
2017-12-08 22:45:19 +0100

    Optimitzed globals variables

--- clearData.py
+++ clearData.py
@@ -13,17 +13,15 @@ count_shopping = 0
 crypto_csv_path = getcwd() + '/csv/crypto_currencies/'
 
 
-def set_date_from_row(csv):
-    global data_raw, match
-    match = re.search(r'\d{1,2}_\d{2}_\d{4}', csv)
+def get_date_from_row(csv):
+    return re.search(r'\d{1,2}_\d{2}_\d{4}', csv)
 
 
-def set_date_value():
-    global date
-    date = datetime.strptime(match.group(), FORMAT_DATA).date()
+def get_date_value(match):
+    return datetime.strptime(match.group(), FORMAT_DATA).date()
 
 
-def add_value_to_array(type_attribute, name, simbol, preu):
+def add_value_to_array(date, type_attribute, name, simbol, preu):
     data_set.append([date, type_attribute.strip(), name.strip(), simbol.strip(), preu])
 
 
@@ -31,11 +29,17 @@ def ls(ruta):
     return [arch.name for arch in scandir(ruta) if arch.is_file()]
 
 
-def add_header_dataset():
+def add_header_data_set():
     data_set.append(HEADERS)
 
 
-def get_value_euros():
+def save_data_set():
+    with open(OUPUT_CSV_PATH, "w+") as f:
+        writer = csv.writer(f)
+        writer.writerows(data_set)
+
+
+def get_value_euros(row):
     value = row[4][1:].replace(',', '.').strip()
     if value:
         return float(value) * EURO_VALUE_FROM_DOLAR
@@ -43,14 +47,12 @@ def get_value_euros():
         return 0
 
 
-def process_files():
-    global f, row
+def process_files_crypto_currency():
     for csv_name in ls(crypto_csv_path):
         process_file_csv(csv_name)
 
 
 def process_file_csv(csv_name):
-    global f, row
     with open(crypto_csv_path + csv_name, newline='') as f:
         print('Process file: ' + csv_name)
         reader = csv.reader(f)
@@ -60,20 +62,11 @@ def process_file_csv(csv_name):
 
 
 def process_row(csv_name, row):
-    set_date_from_row(csv_name)
+    match = get_date_from_row(csv_name)
     if match:
-        set_date_value()
-        add_value_to_array(CRYPTO_MONEDA, row[1], row[2], get_value_euros())
+        add_value_to_array(get_date_value(match), CRYPTO_MONEDA, row[1], row[2], get_value_euros(row))
 
 
-def save_data_set():
-    global f
-    # Any results you write to the current directory are saved as output.
-    with open(OUPUT_CSV_PATH, "w+") as f:
-        writer = csv.writer(f)
-        writer.writerows(data_set)
-
-
-add_header_dataset()
-process_files()
+add_header_data_set()
+process_files_crypto_currency()
 save_data_set()
